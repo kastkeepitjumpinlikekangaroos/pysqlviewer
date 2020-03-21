df1bb05e70955f9bc2fb119a480bb5532b606a03
Owen <owendot2011@gmail.com>
2020-03-21 01:08:02 -0400

    removed unneeded db code

--- db.py
+++ db.py
@@ -4,13 +4,4 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 
 engine = create_engine(os.environ.get('CONNECTION_STRING'))
-session_factory = sessionmaker(bind=engine)
 
-
-@contextmanager
-def get_db():
-    db = scoped_session(session_factory)
-    try:
-        yield db
-    finally:
-        db.close()
